dba5e906afdee4096e72e5a41cea88ca188768b5
Oki Chandra Cristianta <setiadgnml@gmail.com>
2025-03-02 02:11:44 +0700

    moga ga error

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+  return (
+    <div className=''>
+      <h1 className='underline black'>Learn React</h1>
+    </div>
+  )
+}
